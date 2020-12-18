ae52a3ba16edebf6730518190e457936ffeddf06
Nikita_Kob <nikitakobylkin94@gmail.com>
2020-12-18 20:38:50 +0300

    !!!

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,19 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+
+        System.out.println(bean == bean1);
+
+        Cat cat_bean_1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat_bean_1.getName() + " " + cat_bean_1.getAge());
+
+        Cat cat_bean_2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat_bean_2.getName() + " " + cat_bean_1.getAge());
+
+        System.out.println(cat_bean_1 == cat_bean_2);
     }
 }
